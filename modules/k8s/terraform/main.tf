63a8165a06b395785be4239144fef0314dca5d6a
Olfi01 <flom@meyer-alpers.de>
2023-06-02 17:52:23 +0200

    Make agent pod cluster admin

--- modules/k8s/terraform/main.tf
+++ modules/k8s/terraform/main.tf
@@ -24,21 +24,69 @@ resource "kubernetes_secret" "tfc_agent_token" {
   }
 }
 
-resource "kubernetes_pod" "tfc_agent" {
+resource "kubernetes_service_account" "tfc_agent" {
+  metadata {
+    name = "tfc-agent"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tfc_agent" {
+  metadata {
+    name = "tfc-agent-role-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.tfc_agent.metadata.0.name
+    namespace = kubernetes_namespace.terraform.metadata.0.name
+  }
+}
+
+resource "kubernetes_deployment" "tfc_agent" {
   metadata {
     namespace = kubernetes_namespace.terraform.metadata.0.name
     name      = "tfc-agent"
+
+    labels = {
+      deployment = "tfc-agent"
+    }
   }
+
   spec {
-    container {
-      name  = "agent"
-      image = "hashicorp/tfc-agent:latest"
-      env {
-        name = "TFC_AGENT_TOKEN"
-        value_from {
-          secret_key_ref {
-            name = kubernetes_secret.tfc_agent_token.metadata.0.name
-            key  = keys(kubernetes_secret.tfc_agent_token.data).0
+    replicas = 1
+
+    selector {
+      match_labels = {
+        deployment = "tfc-agent"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          deployment = "tfc-agent"
+        }
+      }
+
+      spec {
+        service_account_name = kubernetes_service_account.tfc_agent.metadata.0.name
+
+        container {
+          name  = "agent"
+          image = "hashicorp/tfc-agent:latest"
+
+          env {
+            name = "TFC_AGENT_TOKEN"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.tfc_agent_token.metadata.0.name
+                key  = keys(kubernetes_secret.tfc_agent_token.data).0
+              }
+            }
           }
         }
       }
