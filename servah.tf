488f9e2cc465c80a6ee24de59328a482b0f69373
Florian Meyer <flom@meyer-alpers.de>
2023-12-26 13:22:36 +0100

    Add kustomization provider config

--- servah.tf
+++ servah.tf
@@ -26,6 +26,10 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
     }
+    kustomization = {
+      source  = "kbst/kustomization"
+      version = "0.9.0"
+    }
   }
 }
 
@@ -48,6 +52,10 @@ provider "proxmox" {
 
 provider "kubectl" {
   load_config_file = false
+}
+
+provider "kustomization" {
+  kubeconfig_incluster = true
 }
 
 module "proxmox_kubernetes_cluster" {
