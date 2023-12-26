e0a338020731a586eeb203a9689769783941c162
Florian Meyer <flom@meyer-alpers.de>
2023-12-26 13:17:30 +0000

    Revert "Add Argocd (#48)"
    This reverts commit 994d01e355b1fcdaf2aab004817575ccf838047b.

--- modules/k8s/in_cluster/main.tf
+++ modules/k8s/in_cluster/main.tf
@@ -12,10 +12,6 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
     }
-    kustomization = {
-      source  = "kbst/kustomization"
-      version = "0.9.0"
-    }
   }
 }
 
@@ -39,42 +35,4 @@ module "github_actions" {
   source = "./github_actions"
 
   github_pat_arc = var.github_pat_arc
-}
-
-data "kustomization_build" "argocd" {
-  path = "${path.module}/argocd"
-}
-
-resource "kustomization_resource" "argocd_p0" {
-  for_each = data.kustomization_build.argocd.ids_prio[0]
-
-  manifest = (
-    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-    ? sensitive(data.kustomization_build.argocd.manifests[each.value])
-    : data.kustomization_build.argocd.manifests[each.value]
-  )
-}
-
-resource "kustomization_resource" "argocd_p1" {
-  for_each = data.kustomization_build.argocd.ids_prio[1]
-
-  manifest = (
-    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-    ? sensitive(data.kustomization_build.argocd.manifests[each.value])
-    : data.kustomization_build.argocd.manifests[each.value]
-  )
-
-  depends_on = [kustomization_resource.argocd_p0]
-}
-
-resource "kustomization_resource" "argocd_p2" {
-  for_each = data.kustomization_build.argocd.ids_prio[2]
-
-  manifest = (
-    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-    ? sensitive(data.kustomization_build.argocd.manifests[each.value])
-    : data.kustomization_build.argocd.manifests[each.value]
-  )
-
-  depends_on = [kustomization_resource.argocd_p1]
 }
