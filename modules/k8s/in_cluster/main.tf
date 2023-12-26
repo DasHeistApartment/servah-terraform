48f60d6a79380eead988461f14b703fc3b7f2417
Florian Meyer <flom@meyer-alpers.de>
2023-12-27 00:58:14 +0100

    Update main.tf

--- modules/k8s/in_cluster/main.tf
+++ modules/k8s/in_cluster/main.tf
@@ -38,8 +38,6 @@ module "github_actions" {
 }
 
 resource "kubernetes_ingress_v1" "argocd" {
-  depends_on = [ kubectl_manifest.argocd ]
-
   metadata {
     name      = "argo-cd"
     annotations = {
