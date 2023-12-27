d563b78f8aaf531d7cd856f61a6d4031082c89cf
Florian Meyer <flom@meyer-alpers.de>
2023-12-27 01:25:47 +0100

    Make terraform pod ignore errored runs

--- modules/k8s/in_cluster/terraform/main.tf
+++ modules/k8s/in_cluster/terraform/main.tf
@@ -89,7 +89,7 @@ resource "kubernetes_deployment" "tfc_agent" {
         container {
           name  = "agent"
           image = "hashicorp/tfc-agent:latest"
-          args  = ["-single"]
+          command = "/home/tfc-agent/bin/tfc-agent -single | tail -f /dev/null"
 
           env {
             name = "TFC_AGENT_TOKEN"
