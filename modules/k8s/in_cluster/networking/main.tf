4eefef9490d4e91ffa45c6f36580877d82d40002
Florian Meyer <flom@meyer-alpers.de>
2023-12-30 15:56:28 +0100

    Make portforward listen

--- modules/k8s/in_cluster/networking/main.tf
+++ modules/k8s/in_cluster/networking/main.tf
@@ -175,6 +175,10 @@ resource "kubernetes_deployment" "portforward" {
             name  = "WEBSERVER_PORT"
             value = 80
           }
+          env {
+            name  = "WEBSERVER_HOST"
+            value = "0.0.0.0"
+          }
           port {
             container_port = 80
             name           = "http"
