71446157d5730065dcfb9525ba373f063346ae78
Florian Meyer <flom@meyer-alpers.de>
2023-12-30 00:24:28 +0000

    Build Kustomization

---
+++ modules/k8s/in_cluster/argocd/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kubectl kustomize . > kustomized.yaml
+rm -r build
+mkdir build
+cp kustomized.yaml build/kustomized.yaml
+cd build
+yq -s '.metadata.name + ".yaml"' kustomized.yaml
+rm kustomized.yaml
