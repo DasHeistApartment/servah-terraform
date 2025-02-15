aaf40d476d7324f7b01d2f282f053fa2a49fa652
Olfi01 <flom@meyer-alpers.de>
2025-02-16 00:16:52 +0100

    update image name in help comment

--- servah.tf
+++ servah.tf
@@ -39,7 +39,7 @@ provider "proxmox" {
 # Will run in-cluster using a service account if the agent is already set up and will therefore need no credentials.
 # If the agent is not yet running, make sure a temporary agent has access to the cluster by defining the KUBE_CONFIG_PATH variable.
 # A command to do this on windows can be found below (make sure docker is installed and running).
-# docker run --name tfc_agent_temp --env TFC_AGENT_TOKEN=%TOKEN% --env "KUBE_CONFIG_PATH=/home/tfc-agent/.kube/config" --mount type=bind,source="%userprofile%\.kube",target=/home/tfc-agent/.kube,readonly hashicorp/tfc-agent:latest
+# docker run --name tfc_agent_temp --env TFC_AGENT_TOKEN=%TOKEN% --env "KUBE_CONFIG_PATH=/home/tfc-agent/.kube/config" --mount type=bind,source="%userprofile%\.kube",target=/home/tfc-agent/.kube,readonly olfi01/custom-tfc-agent:latest
 #}
 
 #provider "helm" {
