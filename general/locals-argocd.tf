af23d0f69f0273699f3781f888b4066bed0766fb
Florian Meyer <flom@meyer-alpers.de>
2025-09-07 15:59:56 +0200

    chore: argocd dex secret and ssl passthrough (#135)

--- general/locals-argocd.tf
+++ general/locals-argocd.tf
@@ -1,4 +1,9 @@
 locals {
   argocd_root_project_content = file("${path.module}/argocd/root_project.yaml")
   argocd_root_app_content     = file("${path.module}/argocd/root_app.yaml")
+  argocd_dex_secret = templatefile("${path.module}/argocd/dex-secret.yaml",
+    {
+      argocd_github_app_secret = var.argocd_github_app_secret
+    }
+  )
 }
--- general/secrets.tf
+++ general/secrets.tf
@@ -12,3 +12,8 @@ variable "ssh_private_key" {
   type      = string
   sensitive = false // set to false to enable log from kubespray provisioner
 }
+
+variable "argocd_github_app_secret" {
+  type      = string
+  sensitive = true
+}
